26b2765be2aca1257c8c38a76c84b20fc8df28d8
Tyler <tylermwight@gmail.com>
2025-10-02 18:13:47 +0000

    day2p1 fix

--- 2024/day_2/part1.groovy
+++ 2024/day_2/part1.groovy
@@ -1,31 +1,47 @@
 List<String> file_contents = new File('./puzzleinput').readLines()
-List<Integer> line_list = []
+List<List<Integer>> line_list = []
 
 
 //populate left/right list
 for(line in file_contents){
-    line_list.add((line.trim().split(/\s+/)).toInteger())
+    line_list.add(line.trim().split(/\s+/).collect { it.toInteger() })
     
 }
 
 
-// def safe_reports = 0
-// line_list.each{row->
-//     println(row)
-//     def prev_level = row[0]
-//     def row_safe = true
-    
-//     row.each{level->
-//         if (Math.abs(level - prev_level) > 2){
-//             row_safe = false
-//             println("unsafe")
-//             return
-//         }
-//     }
-//     if (row_safe == true){
-//         safe_reports = safe_reports + 1
-//     }
 
-// }
+boolean is_safe(List<Integer> row){
+    boolean increasing = null
 
-// println("Answer (safe_reports) = " + safe_reports)
+    for (int i = 1; i < row.size(); i++){
+        def difference = row[i] - row[i-1]
+
+        if (difference == 0 || Math.abs(difference) > 3){
+                println("unsafe: diff = " + difference)
+                return false
+            }
+
+        if (i == 1){
+            increasing = difference > 0
+        } else{
+            if ((difference > 0) != increasing){
+                println("unsafe: switched signs")
+                return false
+                }
+        }
+    }
+
+    return true
+}
+
+def safe_reports = 0
+line_list.each{row->
+    println(row)
+    if (is_safe(row)){
+        println("safe")
+        safe_reports = safe_reports + 1
+    }
+
+}
+
+println("Answer (safe_reports) = " + safe_reports)
