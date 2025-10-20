7307b7df2342e00c78ddf3867c8027fb43caa45b
Tyler <tylermwight@gmail.com>
2025-10-20 18:17:15 +0000

    day3p2 working

--- 2024/day_3/part2.groovy
+++ 2024/day_3/part2.groovy
@@ -3,14 +3,31 @@ List<String> file_contents = new File('./puzzleinput').readLines()
 def answer = 0
 boolean enabled = true
 
-def pattern = ~/mul\((\d+),(\d+)\)/
+def pattern = ~/(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)/
+
 
 for(line in file_contents){
 
-    def muls = pattern.matcher(line)
+    def matches = pattern.matcher(line)
 
-    while (muls.find()){
-        print(muls.group(0) + "\n\n")
+    while (matches.find()){
+        def working_string = matches.group(0)
+
+        if (working_string == "do()"){
+            enabled = true
+
+        } else if (working_string == "don't()"){
+            enabled = false
+            
+        } else if (enabled == true){
+            def mul = (working_string =~ /mul\((\d+),(\d+)\)/)
+            mul.find()
+            println(mul.group(0))
+            def a = mul.group(1) as int 
+            def b = mul.group(2) as int
+            println("A: ${a}  B: ${b}")
+            answer = answer + (a *b)
+        }
     }
     
 }
